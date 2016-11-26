09e5227d27ebad60425a3da811afb37bdc48782b
Nicolas BLANC <nicolas.r.blanc@gmail.com>
2016-11-26 01:17:43 -0500

    Add TeamManager

---
+++ TeamManager.cpp
@@ -0,0 +1,45 @@
+#include <cstdlib>
+
+#include "TeamManager.h"
+#include "misc\Cgdi.h"
+
+TeamManager* TeamManager::s_singleton = NULL;
+
+TeamManager::TeamManager()
+{
+	s_singleton = this;
+ 	m_NumberOfBot = 0;
+	m_Team = new std::map<int, std::vector<Raven_Bot*>>();
+	offsetColor = std::rand() % gdi->NumPenColors();
+}
+
+TeamManager::~TeamManager()
+{
+}
+
+void TeamManager::RegisterTeam(Raven_Bot * bot)
+{
+	int team = m_NumberOfBot % m_NumberOfTeam;
+	if (m_Team->count(team) == 0)
+	{
+		std::vector<Raven_Bot*> newTeam;
+		newTeam.push_back(bot);
+		m_Team->insert(std::make_pair(team, newTeam));
+	}
+	else
+	{
+		m_Team->at(team).push_back(bot);
+	}
+
+	m_NumberOfBot++;
+	bot->SetTeam(team);
+}
+
+void TeamManager::UnregisterTeam(Raven_Bot * bot)
+{
+}
+
+int TeamManager::getColor(int team)
+{
+	return (offsetColor + team) % gdi->NumPenColors();
+}
